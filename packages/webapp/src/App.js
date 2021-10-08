daf3c6e1a2196cbccdf3d2551d5573e810f39f94
eldnctlh <cequeditlabouchedombre@gmail.com>
2021-10-08 15:22:50 +0300

    initial commit

---
+++ packages/webapp/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import {Button} from "@shared/library";
+
+function App() {
+  return (
+      <Button>Peoplesafe library Button</Button>
+  );
+}
+
+export default App;
